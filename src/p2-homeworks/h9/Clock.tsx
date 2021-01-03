9b0abaf8a0eb190a45b7dc5fe7900aa2af431321
Natasha-eng <natasha.shilova.89@gmail.com>
2021-01-03 17:51:57 +0300

    created digital clocks, used clearInterval

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,46 +1,58 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
+import c from "./Clock.module.css"
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0);
-    const [date, setDate] = useState<Date>();
+    const [date, setDate] = useState<Date>(new Date());
     const [show, setShow] = useState<boolean>(false);
 
     const stop = () => {
-        // stop
+        clearInterval(timerId)
     }
+
     const start = () => {
-        stop();
         const id: number = window.setInterval(() => {
-            // setDate
+            //setDate
+            setDate(new Date());
+
         }, 1000);
         setTimerId(id);
+        return () => {
+            clearInterval(id)
+        }
     }
 
     const onMouseEnter = () => {
         // show
+        setShow(true)
     };
     const onMouseLeave = () => {
         // close
+        setShow(false)
     };
 
-    const stringTime = "Time"; // fix with date
-    const stringDate = "Date"; // fix with date
+    const twoDigits = (num: number) => num < 10 ? '0' + num : num;
+    //const stringTime = new Date().toLocaleTimeString() //"Time"; // fix with date
+    const stringTime = twoDigits(date.getHours()) + '-' + twoDigits(date.getMinutes()) + '-' + twoDigits(date.getSeconds());
+    const stringDate = new Date().toLocaleDateString()  //"Date"; // fix with date
 
     return (
         <div>
-            <div
-                onMouseEnter={onMouseEnter}
-                onMouseLeave={onMouseLeave}
-            >
-                {stringTime}
-            </div>
-
-            {show && (
-                <div>
-                    {stringDate}
+            <div className={c.time}>
+                <div
+                    onMouseEnter={onMouseEnter}
+                    onMouseLeave={onMouseLeave}
+                >
+                    {stringTime}
                 </div>
-            )}
+
+                {show && (
+                    <div>
+                        {stringDate}
+                    </div>
+                )}
+            </div>
 
             <SuperButton onClick={start}>start</SuperButton>
             <SuperButton onClick={stop}>stop</SuperButton>
--- src/p2-homeworks/h9/HW9.tsx
+++ src/p2-homeworks/h9/HW9.tsx
@@ -1,18 +1,16 @@
 import React from "react";
 import Clock from "./Clock";
+import c from "../h5/HW5.module.css";
 
 function HW9() {
     return (
-        <div>
+        <div className={c.Chapter}>
             <hr/>
-            homeworks 9
+            <div className={c.NameChapter}>homeworks 9</div>
 
             {/*should work (должно работать)*/}
             <Clock/>
 
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeClock/>*/}
             <hr/>
         </div>
     );
